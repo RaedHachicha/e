b3765c3c5accd3382b134b1e7bc40562fbe070df
Raed Hachicha <76795372+RaedHachicha@users.noreply.github.com>
2023-02-17 16:42:10 +0100

    Master

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'package:e_cinemav1/authentification/view/register_view.dart';
+import 'package:e_cinemav1/consts/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:provider/provider.dart';
+
+import 'authentification/services/authstate_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,13 +15,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthStateProvider()),
+      ],
+      child: GetMaterialApp(theme: regularTheme, home: RegisterUI()),
     );
   }
 }
-
